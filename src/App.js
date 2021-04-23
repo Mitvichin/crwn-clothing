e31126a77b5cf1a79dea0a9733208207d317bfc6
Ilia Mitvichin <mitvichin@gmai.com>
2021-04-23 17:55:03 +0100

    completed section 6

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
-import { HomePage } from "./pages/homepage/homepage.component";
+import { HomePage } from "./pages/home/home-page.component";
 import { Route, Switch } from "react-router-dom";
-
-const Asd = () => <div>Hats</div>;
+import ShopPage from "./pages/shop/shop-page.component";
+import Header from "./components/header/header.component";
+import SignInAndSignOutPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up-page.component";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={Asd} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignOutPage} />
       </Switch>
     </div>
   );
