a48504fd54805e85b0fc5ce17e9de4a1deb5fa30
Ilia Mitvichin <mitvichin@gmai.com>
2021-04-23 14:26:56 +0100

    completed section 5

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { sections } from "./sections";
 import "./directory.styles.scss";
-import { MenuItem } from "../menu-item/menu-item.component";
+import MenuItem from "../menu-item/menu-item.component";
 
 class Directory extends React.Component {
   constructor() {
@@ -14,8 +14,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, id, imageUrl, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
