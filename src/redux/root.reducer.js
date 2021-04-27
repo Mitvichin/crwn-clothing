0e66ffc2f48b1329e693ddda40e1eb0af71a52ec
Ilia Mitvichin <mitvichin@gmai.com>
2021-04-27 13:15:11 +0100

    completed section 9

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,5 +1,15 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
 
-export default combineReducers({ user: userReducer, cart: cartReducer });
+const persistConfig = {
+  key: "root",
+  storage,
+  whiteList: ["cart"],
+};
+
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+
+export default persistReducer(persistConfig, rootReducer);
